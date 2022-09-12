b5e6f008ddfe88ec0629074d09f345ae597bded8
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-11 22:22:42 -0300

    add dados de times

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 
+const teams = [
+    {
+      id: 1,
+      name: 'São Paulo Futebol Clube',
+      initials: 'SPF',
+    },
+    {
+      id: 2,
+      name: 'Clube Atlético Mineiro',
+      initials: 'CAM',
+    },
+  ];
+
 const app = express();
 
+// app.get('/teams', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 module.exports = app;
